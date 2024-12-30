2df7f8ddca4f3a49fac7fdaafa0258a2ce138052
rprakashdass <rprakashdass@gmail.com>
2024-12-30 16:59:21 +0530

    init configurations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import {FlipCard} from "./pages/";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-center">App</div>
+      <FlipCard/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import Login from "./Login/Login";
+import FlipCard from "./FlipCard/FlipCard";
+
+export {Login};
+export {FlipCard};
